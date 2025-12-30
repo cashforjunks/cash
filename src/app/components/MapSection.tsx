765253405671f1b10c1c9f8f9cf26b0fe1670681
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2025-12-30 15:08:22 +0300

    Update MapSection.tsx

--- src/app/components/MapSection.tsx
+++ src/app/components/MapSection.tsx
@@ -11,8 +11,7 @@ export function MapSection() {
           loading="lazy"
           allowFullScreen
           referrerPolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!2d-87.6744!3d41.6005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDM2JzQ2LjAiTiA4N8KwNDAnMTIuMCJX!5e0!3m2!1sen!2sus!4v000000000000000000000"
-        ></iframe>
+          src=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.203100230082!2d-87.79377099999999!3d41.802385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e374b6e3a30a7%3A0xdce958c0def29e04!2sI55%20TRUCK%20PARKING!5e0!3m2!1sen!2sjo!4v1767096314979!5m2!1sen!2sjo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </section>
   );
