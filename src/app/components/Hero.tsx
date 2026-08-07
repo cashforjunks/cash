0930ca471ca76d6e02153fddb91fcb08d78f54f9
cashforjunks <carsjunk81@gmail.com>
2026-08-07 22:04:39 +0300

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -70,9 +70,9 @@ Get an accurate offer in just minutes.
             className="bg-white/10 backdrop-blur-sm border-white text-white hover:bg-white/20 px-8 py-6"
             asChild
           >
-            <a href="tel:+17089791549">
+            <a href="tel:+17088866282">
               <Phone className="mr-2 h-5 w-5" />
-              Call: (708) 998-1730
+              Call: (708) 886-6282
             </a>
           </Button>
         </div>
