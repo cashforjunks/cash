534697b9a6743f953402591b08b05d40d258c770
cashforjunks <carsjunk81@gmail.com>
2026-09-01 17:21:28 +0300

    Update MapSection.tsx

--- src/app/components/MapSection.tsx
+++ src/app/components/MapSection.tsx
@@ -13,7 +13,7 @@ export function MapSection() {
         <div className="w-full h-[450px] lg:h-[550px] rounded-lg overflow-hidden shadow-lg">
           <iframe
             title="Cash For Junks Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.203100230082!2d-87.79377099999999!3d41.802385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e374b6e3a30a7%3A0xdce958c0def29e04!2sI55%20TRUCK%20PARKING!5e0!3m2!1sen!2sjo!4v1767096314979!5m2!1sen!2sjo"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2982.515990199985!2d-87.548118!3d41.622977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e21dc345069ed%3A0x1cb3b088c9d51f8c!2sQuick%20CASH%20JUNK%20CARS%20LLC!5e0!3m2!1sen!2sjo!4v1788272424036!5m2!1sen!2sjo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"
             width="100%"
             height="100%"
             style={{ border: 0 }}
